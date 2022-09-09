c6724fe9115f0402a549017674a85ec6e12ac268
Huy <wanghcr11@gmail.com>
2022-09-09 10:35:06 +0700

    Update asm2.asm

--- Bai tap/Week 1/Đỗ Quang Huy/Window/asm2.asm
+++ Bai tap/Week 1/Đỗ Quang Huy/Window/asm2.asm
@@ -1,17 +1,16 @@
-.model small
+include \masm32\include\masm32rt.inc
 
-.stack 100h
 .data
-    tb1 db "Nhap xau: $"
-    tb2 db "Xau vua nhap $"
-    string db ?
+    st1 DB "Nhap xau: ",0
+    st2 DB "Xau vua nhap: ", 0
+    st3 DB ?
+
 .code
-
-nhapxau proc
-    ;write your code here
-nhapxau endp
-
 main:
-    mov ax, @data
-    INVOKE nhapxau
-end main
+    print OFFSET st1
+    mov st3, input()
+    print OFFSET st2
+    print st3
+    exit
+ end main
+        
