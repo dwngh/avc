da633a7c0ad3c520e1cc7bcf093a18c8569239b5
Huy <wanghcr11@gmail.com>
2022-09-11 19:03:03 +0700

    Update bai 2 tuan 1

--- Bai tap/Week 1/Đỗ Quang Huy/Window/asm2.asm
+++ Bai tap/Week 1/Đỗ Quang Huy/Window/asm2.asm
@@ -1,16 +1,15 @@
 include \masm32\include\masm32rt.inc
 
 .data
-    st1 DB "Nhap xau: ",0
-    st2 DB "Xau vua nhap: ", 0
-    st3 DB ?
+buffer   db "This is a buffer"
+   db 0
 
 .code
-main:
-    print OFFSET st1
-    mov st3, input()
-    print OFFSET st2
-    print st3
-    exit
- end main
-        
+start:
+   print chr$("Nhap xau: ")
+   invoke StdIn, addr buffer, sizeof buffer
+   print chr$("Xau vua nhap: ")
+   print offset buffer, 13, 10
+   exit
+
+end start
